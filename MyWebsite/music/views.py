49de4c041d88c80c05f6b4671ad2e4dd76e29132
Saroj Kunwar <sarojkunwar999@gmail.com>
2018-07-02 17:48:55 +0545

    Update views.py
    data base connection and raising HTTP 404 ERROR

--- MyWebsite/music/views.py
+++ MyWebsite/music/views.py
@@ -1,6 +1,23 @@
-from django.http import HttpResponse
+from django.http import Http404
+from .models import Album
+from django.shortcuts import render
 
+
+#working with templates
 
 def index(request):
-    return HttpResponse("<h1>i am a noobie programmer<h1>")
+    all_albums = Album.objects.all()
+
+    context = {'all_albums': all_albums}
+    return render(request, 'music/index.html', context)
+
+
+def detail(request, album_id):
+    try:
+       album=Album.objects.get(id=album_id)
+    except Album.DoesNotExist:
+       raise Http404("Album doesnot exist!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    return render(request, 'music/detail.html', {'album': album})
+
+
 
