00455b162580d2a6b6317c77ca824dbc59ce4a77
Saroj Kunwar <sarojkunwar999@gmail.com>
2018-07-02 17:50:41 +0545

    Update urls.py
    Updated url.py for database connections

--- MyWebsite/music/urls.py
+++ MyWebsite/music/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # /music/
+    path('', views.index, name='index'),
+
+    # /music/71
+    path('<int:album_id>/', views.detail, name='detail'),
+
 ]
